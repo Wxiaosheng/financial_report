05b2e7b2d603e7684a21a128a69e1a22c5088f34
xiaosheng <287915338@qq.com>
2020-02-07 12:54:27 +0800

    第一版上线

---
+++ build/precache-manifest.07da2792f69ad212b8146282c678ee7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07713c3a8e25f37180f698bf50bf9931",
+    "url": "/financial_report_admin/index.html"
+  },
+  {
+    "revision": "5241a9a1c60bdcb8a3c8",
+    "url": "/financial_report_admin/static/css/2.b19d5071.chunk.css"
+  },
+  {
+    "revision": "90f617ddb8d23da072c2",
+    "url": "/financial_report_admin/static/css/main.57f82cb6.chunk.css"
+  },
+  {
+    "revision": "5241a9a1c60bdcb8a3c8",
+    "url": "/financial_report_admin/static/js/2.3cdf480a.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/financial_report_admin/static/js/2.3cdf480a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f617ddb8d23da072c2",
+    "url": "/financial_report_admin/static/js/main.aba4f825.chunk.js"
+  },
+  {
+    "revision": "f8ece79e8be478fa23a3",
+    "url": "/financial_report_admin/static/js/runtime-main.b1245b71.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.175959dcef2dd0b9eaa0971273905222.js"
+  "/financial_report_admin/precache-manifest.07da2792f69ad212b8146282c678ee7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/financial_report_admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
